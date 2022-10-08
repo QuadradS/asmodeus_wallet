59b3ef720da8d86c55d07ea7130cf7f96393ca09
Danil Sitdikov <quadrad4@gmail.com>
2022-10-08 20:42:44 +0600

    feat - deposit feat - balance of feat - string > bytes32 username

--- contracts/Main.sol
+++ contracts/Main.sol
@@ -6,23 +6,39 @@ import "hardhat/console.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract Main is Ownable {
+
   //Users
   struct UserAccount {
-    string name;
+    bytes32 name;
     address wallet;
+    int256 amount;
+    uint created;
   }
 
   mapping (address => UserAccount) public users;
+
+  modifier _onlyExists {
+    require(users[msg.sender].wallet != address(0), 'Account is not exist');
+    _;
+  }
   //Users
 
   UserAccount public ownerAccount;
 
-  constructor(string memory name){
-    ownerAccount = UserAccount(name, msg.sender);
+  constructor(bytes32 name){
+    ownerAccount = UserAccount(name, msg.sender, 0, block.timestamp);
   }
 
-  function setUser(string memory _name, address _address) public onlyOwner {
-    users[_address] = UserAccount(_name, _address);
+  function setUser(bytes32 _name, address _address) public onlyOwner {
+    users[_address] = UserAccount(_name, _address, 0, block.timestamp);
+  }
+
+  function deposit(int256 amount) public _onlyExists {
+    users[msg.sender].amount = users[msg.sender].amount + amount;
+  }
+
+  function userBalance(address _address) public view returns(int256) {
+    return users[_address].amount;
   }
 
 
